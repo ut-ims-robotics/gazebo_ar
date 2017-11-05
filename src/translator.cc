b52c967e1a5da2bcf279d757441e9303aa07387f
Mateus Reis <mateus.sureis@gmail.com>
2017-11-05 02:25:37 +0200

    Rough directions and distances, better launch file

--- src/translator.cc
+++ src/translator.cc
@@ -12,23 +12,30 @@
 
 gazebo::msgs::Pose last_pose;
 
-void convertToGzPose(const geometry_msgs::Pose& rosPosePtr) {
+void convertToGzPose(const geometry_msgs::Pose& marker_pose) {
 //MAYBE TODO: Stuff all this in a class so I don't need globals
-   ignition::math::Pose3<double> intermed(
-      rosPosePtr.position.x,
-      rosPosePtr.position.y,
-      rosPosePtr.position.z,
-      rosPosePtr.orientation.x,
-      rosPosePtr.orientation.y,
-      rosPosePtr.orientation.z,
-      rosPosePtr.orientation.w
-      );
+   ignition::math::Quaternion<double> cam_quaternion(/*1,0,0,0*/
+      marker_pose.orientation.w,
+      marker_pose.orientation.z,
+      -marker_pose.orientation.x,
+      marker_pose.orientation.y
+   );
+   ignition::math::Vector3<double> cam_position(
+      marker_pose.position.z,
+      -marker_pose.position.x,
+      marker_pose.position.y + 0.5
+   );
+   /* ignition::math::Quaternion<double> z_quaternion(0, 0, 3.14159); */
+   /* cam_quaternion *= z_quaternion; */
+   ignition::math::Pose3<double> intermed(cam_position, cam_quaternion);
+
    last_pose = gazebo::msgs::Convert(intermed);
 }
 
 void artagReceived(const visualization_msgs::Marker::ConstPtr& msg) {
    ROS_INFO("CALLBACK RECEIVED");   
    convertToGzPose(msg->pose);
+   /* last_pose = msg->pose; */
 }
 
 int main(int _argc, char **_argv)
@@ -42,13 +49,16 @@ int main(int _argc, char **_argv)
 
    //ros startup
    ros::init(_argc, _argv, "gazebo_ar");
-   ros::NodeHandle rosNode;
-   ros::Subscriber rosSub = rosNode.subscribe("visualization_marker", 100, artagReceived);
+   ros::NodeHandle ros_node;
+   ros::Subscriber ros_sub = ros_node.subscribe("visualization_marker", 100, artagReceived);
+   /* ros::Publisher ros_pub = ros_node.advertise("gazebo/set_model_state", 100); */
    ROS_INFO("ROS Setup done");
    ros::Rate r(100);
 
 	while (ros::ok()) {
 		gzCamPub->Publish(last_pose);
+      /* ros_pub.publish */
+
       ros::spinOnce();
       r.sleep();
 	}
